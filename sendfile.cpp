62b350523b973c0b06a218d75ed01da52772c217
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2017-10-16 21:26:52 +0700

    Finish handle user input parameter, add gitignore

--- sendfile.cpp
+++ sendfile.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <stdio.h>
 #include <netdb.h>
 #include <stdlib.h>
@@ -7,12 +8,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 using namespace std;
 
+#define BUFFER_SIZE	    2048
+#define IP_BUFFER_SIZE  15
 #define SERVICE_PORT	21234
 
 int main(int argc, char *argv[]) {
+
+    char filename[BUFFER_SIZE];
+    char destinationIP[IP_BUFFER_SIZE];
+    int windowsize;
+    int buffersize;
+    int destinationPort;
 
     // Handle user parameter
     if (argc == 2 && memcmp("-h", argv[1], 2) == 0) {
@@ -20,16 +28,23 @@ int main(int argc, char *argv[]) {
         cout << "   Execute this program with this command:" << endl;
         cout << "   ./sendfile <filename> <windowsize> <buffersize> <destination_ip> <destination_port>" << endl << endl;
         exit(0);
-    } else if (argc != 5) {
-        cout << "Wrong parameter!" << endl;
+    } else if (argc != 6) {
+        cout << "   Wrong parameter! use -h for instruction" << endl;
         exit(0);
+    } else {
+        strcpy(filename, argv[1]);
+        windowsize = atoi(argv[2]);
+        buffersize = atoi(argv[3]);
+        strcpy(destinationIP, argv[4]);
+        destinationPort = atoi(argv[5]);
     }
     
     int fd;
     struct sockaddr_in address, remoteAddress;
     struct hostent *host;
-    char message[2048];
-    char server[15] = "127.0.0.1";
+    char message[BUFFER_SIZE];
+    char server[IP_BUFFER_SIZE];
+    strcpy(server, destinationIP);
    
     // Creating UDP socket
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -55,15 +70,16 @@ int main(int argc, char *argv[]) {
     // Defining remote client
     memset((char* ) &remoteAddress, 0, sizeof(remoteAddress));
     remoteAddress.sin_family = AF_INET;
-    remoteAddress.sin_port = htons(SERVICE_PORT);
+    remoteAddress.sin_port = htons(destinationPort);
     if (inet_aton(server, &remoteAddress.sin_addr) == 0) {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
 
     // Sending messages
+    cout << "Sending file " << filename << " to " << destinationIP << ":" << destinationPort << " (" << windowsize << ", " << buffersize << ")" << endl;
     for (int i = 0; i < 5; i++) {
-        printf("Sending packet %d to %s port %d\n", i, server, SERVICE_PORT);
+        printf("Sending packet %d to %s port %d\n", i, server, destinationPort);
         sprintf(message, "Hello network! %d", i);
         if (sendto(fd, message, strlen(message), 0, (struct sockaddr *) &remoteAddress, sizeof(remoteAddress)) == -1)
             perror("sendto error");
